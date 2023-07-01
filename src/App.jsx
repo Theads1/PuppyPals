a5e42b0b66f27fd75418cabeaaf1a20b9f4aeb86
Theads1 <tiaradheads@gmail.com>
2023-07-01 13:57:54 -0400

    added usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import {puppyList} from './data.js'
 
 import './App.css'
 
 function App() {
-  console.log(puppyList)
+  // console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
 
   return (
-    <>
-      <div>
-        
+  
+      <div className="App">
+      { 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>;
+   })
+}
       </div>
 
-    </>
   )
 }
 
