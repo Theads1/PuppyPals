41092a86860c2b1b6d9211078b9cec46589519d1
Theads1 <tiaradheads@gmail.com>
2023-07-01 15:01:01 -0400

    updated CSS/colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
-
 import './App.css'
+
 
 function App() {
   // console.log(puppyList)
   const [puppies, setPuppies] = useState(puppyList);
   console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
   
       <div className="App">
-      { 
-   puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>;
-   })
+      { puppies.map((puppy) => {
+
+      return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
+   
+    })
+  
+
 }
+{ featPupId && (
+        <div>
+          <h2 style={{color:'#87CEEB'}}>{featuredPup.name}</h2>
+          <ul style={{color:'beige'}}>
+            <li >Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
+    
 
   )
 }
